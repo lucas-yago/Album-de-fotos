8c2e1c8ee67d2312335c6dd3206df28bc9ef2e56
Lucas-yago <41453379+Lucas-yago@users.noreply.github.com>
2021-12-08 16:33:20 -0300

    instala axios   atualiza app, index e theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,61 @@
 import './App.scss';
+
 // import { createClient } from 'pexels';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import { NavBar } from './components/NavBar';
 import { Footer } from './components/Footer';
 import { Pagination } from './components/Pagination';
-
+import { Gallery } from './components/Gallery';
 
 export const App = () => {
-  // const [Fotos, setPhotos] = useState([]);
+  const [Url, setUrl] = useState("https://api.pexels.com/v1/curated");
+  const [Data, setData] = useState();
+  const [Photos, setPhotos] = useState([]);
+  const [Page, setPage] = useState(1);
 
   // const client = createClient('563492ad6f917000010000010e9b810d92534db68d3947a7985ed9f4');
 
-  // const curatedPhotos = async (perPage) => {
-  //   client.photos.curated({ per_page: perPage }).then((photos) => {
+  // const curatedPhotos = async (perPage , current_page) => {
+  //   client.photos.curated({ per_page: perPage, page: current_page }).then((photos) => {
   //   setPhotos(photos.photos);
-  //   console.log(photos);
-  //   });
+  // });
   // };
 
-  // useEffect(() => {
-  //   curatedPhotos(5);
-  // },[]);
+  const curatedPhotos = (url) => {
+    axios.get(url, {
+      headers: {
+        authorization: '563492ad6f91700001000001a4beb2375b2b461fb9c852f78e1f8df5'
+      },
+      params: {
+        per_page: 20
+      }
+    })
+      .then(res => {
+        setData(res.data)
+        setPhotos(res.data.photos)
+        setPage(res.data.page)
+      })
+      .catch(err => console.log(err))
+  };
 
+  useEffect(() => {
+    curatedPhotos(Url)
+  }, [Url]);
+
+  const handleNextPage = () => {
+    setUrl(Data.next_page)
+  };
+  const handlePrevPage = () => {
+    setUrl(Data.prev_page)
+  };
+
+  console.log(Data);
 
   return (
     <div className="App">
       <NavBar />
-      {/* {Fotos.map((foto, index) => (
-          <div key={index} className="container">
-            <img className='image' src={foto.src.tiny} alt="" />
-            <div className='barrinha'>
-              <p>fotografo: {foto.photographer}</p>
-            </div>
-          </div>
-        ))} */}
+      <Gallery Photos={Photos}/>
       <Pagination
         prevPage={handlePrevPage}
         nextPage={handleNextPage}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import { App } from './App';
-import './themes.scss';
+import './custom.scss';
 import reportWebVitals from './reportWebVitals';
 
 
