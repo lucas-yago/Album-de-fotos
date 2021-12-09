e200ef6f7bd3320ee8843da08d323fb2e99d420f
Lucas-yago <41453379+Lucas-yago@users.noreply.github.com>
2021-12-09 02:57:31 -0300

    adiciona interação da SearchBar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,17 +1,30 @@
 import './styles.scss';
-
+import { useState } from 'react';
 import { InputGroup, FormControl } from "react-bootstrap";
 import { AiOutlineSearch } from "react-icons/ai";
 
 export const SearchBar = () => {
-  return (
 
+  const [InputText, setInputText] = useState('')
+
+  const handleKeyUp = (key) => {
+    if (key.code === 'Enter' && InputText !== '') {
+      console.log(InputText)
+      setInputText('')
+    };
+  };
+
+
+  return (
     <InputGroup >
       <InputGroup.Text className='boxIcon'> <AiOutlineSearch /> </InputGroup.Text>
       <FormControl
         type="text"
         className="inputSearch"
         placeholder="Search ..."
+        value={InputText}
+        onChange={(e) => setInputText(e.target.value)}
+        onKeyUp={handleKeyUp}
       />
     </InputGroup>
   );
